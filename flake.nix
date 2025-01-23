a15ed580700a9ea3123eb410103c4087d7c2d139
Ophestra <random@chars.jp>
2025-01-23 23:18:53 +0900

    ci: implement formatting checks via treefmt-nix
    Signed-off-by: Ophestra <random@chars.jp>

--- flake.nix
+++ flake.nix
@@ -3,10 +3,18 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11-small";
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs =
-    { self, nixpkgs }:
+    {
+      self,
+      nixpkgs,
+      treefmt-nix,
+    }:
     let
       supportedSystems = [
         "aarch64-linux"
@@ -16,6 +24,8 @@
 
       forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
       nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
+
+      treefmtEval = forAllSystems (system: treefmt-nix.lib.evalModule nixpkgsFor.${system} ./treefmt.nix);
     in
     {
       packages = forAllSystems (
@@ -26,6 +36,41 @@
         {
           default = self.packages.${system}.hizla;
           hizla = pkgs.callPackage ./package.nix { };
+        }
+      );
+
+      formatter = forAllSystems (system: treefmtEval.${system}.config.build.wrapper);
+
+      checks = forAllSystems (
+        system:
+        let
+          pkgs = nixpkgsFor.${system};
+        in
+        {
+          formatting = treefmtEval.${system}.config.build.check self;
+
+          lint =
+            let
+              inherit (pkgs) runCommandLocal deadnix statix;
+            in
+            runCommandLocal "check-lint"
+              {
+                nativeBuildInputs = [
+                  deadnix
+                  statix
+                ];
+              }
+              ''
+                cd ${./.}
+
+                echo "running deadnix..."
+                deadnix --fail
+
+                echo "running statix..."
+                statix check .
+
+                touch $out
+              '';
         }
       );
     };
